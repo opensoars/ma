db73ec7ff4ed340affedfe15302ba8eb1d8e35aa
opensoars <sam_1700@hotmail.com>
2014-11-26 16:30:47 +0100

    getMarkdownFiles logWarn dependency fixed

--- test/mocha/getMarkdownFiles.js
+++ test/mocha/getMarkdownFiles.js
@@ -2,7 +2,8 @@ var assert = require('assert');
 
 var base_dir = __dirname + '/../..',
     lib_dir = base_dir + '/lib/',
-    module_dir = base_dir + '/node_modules/';
+    module_dir = base_dir + '/node_modules/'
+    md_dir = __dirname + '/md/';
 
 
 process.LIB = lib_dir;
@@ -17,7 +18,17 @@ var getMarkdownFiles = require(lib_dir + 'getMarkdownFiles.js');
 
 
 describe('#getMarkdownFiles', function (){
-  it('test', function (){
-    getMarkdownFiles();
+
+  it("doesn't throw when we don't pass a dir to read", function (){
+    assert.doesNotThrow(function (){
+      getMarkdownFiles();
+    });
   });
+
+  it("doesn't throw if a dir is empty", function (){
+    assert.doesNotThrow(function (){
+      getMarkdownFiles(md_dir);
+    });
+  });
+
 });
