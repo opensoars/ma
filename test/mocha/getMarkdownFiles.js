c1a33b662a8c7f54ae44ba1bee1eaabba3215f9a
opensoars <sam_1700@hotmail.com>
2014-11-26 16:37:20 +0100

    Extra multi files test included

--- test/mocha/getMarkdownFiles.js
+++ test/mocha/getMarkdownFiles.js
@@ -27,8 +27,26 @@ describe('#getMarkdownFiles', function (){
 
   it("doesn't throw if a dir is empty", function (){
     assert.doesNotThrow(function (){
-      getMarkdownFiles(md_dir);
+      getMarkdownFiles(md_dir + 'empty');
     });
+  });
+
+  it("reads files in a dir with files", function (){
+    assert.doesNotThrow(function (){
+      getMarkdownFiles(md_dir + 'correct');
+    });
+  });
+
+  it("doesn't process files other than markdown files", function (){
+    var files = getMarkdownFiles(md_dir + 'non_md');
+
+    assert.equal(files.length, 1);
+    assert.equal(files[0], 'test.md');
+  });
+
+  it("returns multiple files if present", function (){
+    var files = getMarkdownFiles(md_dir + 'multi');
+    assert.equal(files.length, 2);
   });
 
 });
