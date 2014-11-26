bdd083ec14f03dab2f8300e4af17445065728661
opensoars <sam_1700@hotmail.com>
2014-11-26 22:28:41 +0100

    Started working on logCommentData test

---
+++ test/mocha/logCommentData.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+
+var base_dir = __dirname + '/../..',
+    lib_dir = base_dir + '/lib/';
+
+
+// inline em with a mocha test so we can check returns
+process.log = function (){
+
+};
+
+// inline em with a mocha test so we can check returns
+process.logErr = function (){
+
+};
