01b3f571e13a399c87f5742fc6ab51c043fa22dc
opensoars <sam_1700@hotmail.com>
2014-11-26 16:42:48 +0100

    Made sure files is an array

---
+++ test/mocha/makeFileList.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+
+var base_dir = __dirname + '/../..',
+    lib_dir = base_dir + '/lib/';
+
+
+var makeFileList = require(lib_dir + 'makeFileList.js');
+
+describe('#makeFileList', function (){
+  it("doesn't throw when we do not pass a files array", function (){
+    assert.doesNotThrow(function (){
+      makeFileList();
+    });
+  });
+
+  it("doesn't throw when we pass an empty files array", function (){
+    assert.doesNotThrow(function (){
+      makeFileList([]);
+    });
+  });
+
+});
