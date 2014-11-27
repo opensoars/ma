584e1f6150c42ed72b9264febe7a8e97e7a44433
opensoars <sam_1700@hotmail.com>
2014-11-27 19:33:51 +0100

    Completed getCommentData tests

--- test/mocha/getCommentData.js
+++ test/mocha/getCommentData.js
@@ -13,8 +13,6 @@ var getCommentData = require(lib_dir + 'getCommentData.js');
 
 
 
-
-/*
 describe('#getCommentData', function (){
 
   it("doesn't throw when we pass no arguments", function (){
@@ -23,16 +21,29 @@ describe('#getCommentData', function (){
     });
   });
 
-  it('calls logErr with 1 argument', function (done){
+  it('returns an object with properties: msg and list', function (){
+    var comment_data = getCommentData();
 
-    process.logErr = function (){
-      assert.equal(arguments.length, 1);
-      console.log("WORKD");
-    };
+    assert.equal(typeof comment_data.msg, 'string');
+    assert.equal(comment_data.list.constructor , Array);
+  });
 
-    var logCommentData1 = require(lib_dir + 'getCommentData.js');
-    logCommentData1('a', 'b', 'c', 'd', 'e');
+  it('gives a msg string back with the passed in file name in it', function (){
+    var comment_data = getCommentData('test');
+
+    console.log(comment_data);
+
+    assert.notEqual(comment_data.msg.indexOf('test'), -1);
+  })
+ 
+  it('turns `true` green', function (){
+    var comment_data = getCommentData('test', true, true, true, true);
+    assert.notEqual(comment_data.list[0].indexOf('\x1B[32m'), -1);
+  });
+
+  it('turns `false` red', function (){
+    var comment_data = getCommentData('test', false, false, false, false);
+    assert.notEqual(comment_data.list[0].indexOf('\x1B[31m'), -1);
   });
 
 });
-*/
