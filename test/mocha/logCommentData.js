6aa9deb1f27f4ed2be5aef0bbcfe43b881aa00e8
opensoars <sam_1700@hotmail.com>
2014-11-27 19:05:33 +0100

    Started testing logCommentData

--- test/mocha/logCommentData.js
+++ test/mocha/logCommentData.js
@@ -1,15 +1,32 @@
 var assert = require('assert');
 
 var base_dir = __dirname + '/../..',
+    module_dir = base_dir + '/node_modules/'
     lib_dir = base_dir + '/lib/';
 
+process.cls = require(module_dir + 'opensoars_cls');
 
-// inline em with a mocha test so we can check returns
-process.log = function (){
+process.MSGS = require(lib_dir + 'msgs.js');
 
-};
+var logCommentData = require(lib_dir + 'logCommentData.js');
 
-// inline em with a mocha test so we can check returns
-process.logErr = function (){
 
-};
+
+describe('#logCommentData', function (){
+
+  it('should not throw when we pass no arguments', function (){
+    assert.doesNotThrow(function (){
+      logCommentData();
+    });
+  });
+
+  it('should call logErr with 1 argument', function (done){
+      process.logErr = function (){
+        assert.equal(arguments.lenght, 1);
+        done();
+      };
+
+      logCommentData();
+  });
+
+});
