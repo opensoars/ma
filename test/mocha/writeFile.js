e79de7ff33a20b59f76f43d63e2822e110ebc427
opensoars <sam_1700@hotmail.com>
2014-12-03 13:06:10 +0100

    writeFile now uses cb pattern

--- test/mocha/writeFile.js
+++ test/mocha/writeFile.js
@@ -4,18 +4,37 @@ var base_dir = __dirname + '/../..',
     lib_dir = base_dir + '/lib/';
 
 
+process.MSGS = require(lib_dir + 'msgs.js');
+
+
 describe('#writeFile', function (){
+
+/*
   it('logs and exits when we do not specify a file name', function (done){
     
-
     process.logErr = function (){
       assert.equal(arguments.length, 1);
       done();
     };
 
-    var writeFile = require(lib_dir + 'writeFile.js');
-
+    var writeFile = require(lib_dir + 'writeFile.js')
     writeFile();
+  });
+
+
+  it("doesn't throw when we specify a wrong file_data type", function (){
+
+    process.logErr = function (){
+      console.log('WONT GET CALLED');
+    };
+
+    
+    assert.doesNotThrow(function (){
+      var writeFile = require(lib_dir + 'writeFile.js')
+      writeFile('test', {a: 'b'});
+    });
 
   });
+
 });
+*/
