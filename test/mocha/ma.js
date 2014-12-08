02ecea85a557cd06c65f827af8c73213bd1ae0c2
opensoars <sam_1700@hotmail.com>
2014-12-08 23:39:50 +0100

    Added test suite starts

--- test/mocha/ma.js
+++ test/mocha/ma.js
@@ -2,3 +2,9 @@ var assert = require('assert');
 
 var base_dir = __dirname + '/../..',
     lib_dir = base_dir + '/lib/';
+
+describe('#ma', function (){
+  it('', function (){
+    
+  });
+});
--- test/mocha/processFile.js
+++ test/mocha/processFile.js
@@ -4,3 +4,9 @@ var base_dir = __dirname + '/../..',
     lib_dir = base_dir + '/lib/';
 
 var processFile = require(lib_dir + 'processFile.js');
+
+describe('#processFile', function (){
+  it('', function (){
+    
+  });
+});
