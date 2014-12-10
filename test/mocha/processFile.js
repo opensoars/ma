f253487be2166080f49e0032a514cd3e352dc123
opensoars <sam_1700@hotmail.com>
2014-12-10 23:42:53 +0100

    Fixture test file is being read

--- test/mocha/processFile.js
+++ test/mocha/processFile.js
@@ -5,6 +5,8 @@ var base_dir = __dirname + '/../..',
 
 var processFile = require(lib_dir + 'processFile.js');
 
+var test_file = fs.readFileSync(base_dir + '/test/fixtures/md/correct/test.md');
+
 describe('#processFile', function (){
   it('should write a processed file if everything succeeds', function (){
     
